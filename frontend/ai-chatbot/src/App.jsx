92ef8f1865a3369c74a7f683525078259910224c
ashutosh11111 <64188641+ashutosh11111@users.noreply.github.com>
2025-11-14 10:40:39 +0530

    Updating prod URLS

--- frontend/ai-chatbot/src/App.jsx
+++ frontend/ai-chatbot/src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/chat-stream", {
+      const response = await fetch("https://ai-chatbot-fastapi-react.onrender.com/chat-stream", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input }),
